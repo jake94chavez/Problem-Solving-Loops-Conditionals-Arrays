78744dbceb38475fe6c2025d23607a97dfa5e6a5
Jake Chavez <jake94chavez@gmail.com>
2017-12-06 12:04:43 -0600

    Commit 2 - completed Strings excercises

---
+++ morning_lab_solutions.js
@@ -0,0 +1,102 @@
+/////////////////////////////////////////////////////////////////////////
+// Greetings, Students! Use this .js file as a worksheet for your lab.//
+///////////////////////////////////////////////////////////////////////
+
+// Write your answers to the conceptual questions
+
+// 1. How do we assign a value to a variable?
+// We would use const or let and set that variable equal to a given value. For example... 
+`let name = "Jake";`
+
+// 2. How do we change the value of a variable?
+// We call the variable and use an assignment operator to give it a new value. For example...
+`name = "Edward";`
+
+// 3. How do we assign an existing variable to a new variable?
+// We use a new variable and set it equal to our old variable. For example...
+`let newName = name;`
+
+///////////////////////////////////////////////////
+
+//Write the answers to your String exercises here:
+let firstVariable = "Hello World";
+firstVariable = 23;
+let secondVariable = firstVariable;
+secondVariable = "Hello again!";
+//firstVariable is still 23 because firstVariable is unaffected by changes to secondVariable.
+
+yourName = "Jake";
+console.log("Hello, my name is " + Jake);
+
+
+// Write the answers to your Boolean exercises here:
+
+
+
+
+
+
+
+
+
+// Write the answers to The Farm here:
+
+
+
+
+
+
+
+
+
+// Write the answers to Driver's Ed here:
+
+
+
+
+
+
+
+
+
+// Write the answers to Let's get even here:
+
+
+
+
+
+
+
+
+
+// Write the answers to Give me Five here:
+
+
+
+
+
+
+
+
+
+// Write the answers to Savings account here:
+
+
+
+
+
+
+
+
+
+// Write the answers to Multiples of 3 and 5 here:
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////
